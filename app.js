1959d925d1cf5c679d3a78bae40c947fd7cccc49
Janice Shih <tut@howard.edu>
2023-04-07 20:21:55 -0400

    add cors

--- app.js
+++ app.js
@@ -7,6 +7,14 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+const cors=require("cors");
+const corsOptions ={
+  origin:'*', 
+  credentials:true,            //access-control-allow-credentials:true
+  optionSuccessStatus:200,
+}
+
+app.use(cors(corsOptions)) // Use this after the variable declaration
 
 app.use(logger('dev'));
 app.use(express.json());
--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,44 @@
 var express = require('express');
 var router = express.Router();
 
+const mysql = require("mysql");
+
+const fs = require("fs");
+var config = {
+  host: "prbase.mysql.database.azure.com",
+  user: "prbase",
+  password: "2Axijoll@",
+  database: "prbase",
+  port: 3306,
+  ssl: { ca: fs.readFileSync("DigiCertGlobalRootCA.crt.pem") },
+};
+
+const db = new mysql.createConnection(config);
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send({
     message: 'Hello World'
   });
+});
+
+router.post("/create", (req, res) => {
+  const username = req.body.username;
+  const email = req.body.email;
+  const project = req.body.project;
+  const thickness = req.body.thickness;
+  const pixel = req.body.pixel;
+  const sample = req.body.sample;
+  const rawImages = req.body.rawImages;
+
+  db.query(
+    "INSERT INTO upload_info (username, email, project, thickness, pixel, sample, rawImages) VALUES (?, ?, ?, ?, ?, ?, ?);",
+    [username, email, project, thickness, pixel, sample, rawImages],
+    (err, results, fields) => {
+      if (err) throw err;
+      else console.log("Inserted " + results.affectedRows + " row(s).");
+    }
+  );
 });
 
 module.exports = router;
